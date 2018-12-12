f734a37cf4070eaf85e522faed17a62fdf26fa9e
fjy202 <40914915+fjy202@users.noreply.github.com>
2018-12-12 19:49:52 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
-def run()
+import sys
+import traceback
+def run(text):
+    try:
+        return eval(text)
+    except:
+        try:
+            exec(text)
+        except Exception as e:
+            sys.stderr.write(traceback.print_exception(*sys.exc_info()))
